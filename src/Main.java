96184f4b0e79ec8046333dc3ea6c59aff50864a0
tmdahr <p84800384@gmail.com>
2025-12-01 06:52:16 +0900

    feat:: TODO-1~4 TODO-1 출석부 생성, TODO-2 학생 생성, TODO-3 출석부에 학생 삽입, TODO-4 출석부에 동일한 학생 삽입 구현 및 테스트

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,39 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
 
         // TODO-1 출석부 생성
         // 학생목록이 비어 있는 출석부를 하나 생성합니다.
+        AttendanceBook book = new AttendanceBook();
 
         // TODO-2 학생 생성
         // 출석횟수가 0회인 2학년 3반 9번 이밤돌을 생성합니다.
         // 출석횟수가 3회인 1학년 1반 11번 김이로를 생성합니다.
         // 출석횟수가 2회인 1학년 2반 9번 최시나를 생성합니다.
+        Student st1 = new Student("이밤돌", 20309, 0);
+        Student st2 = new Student("김이로", 10111, 3);
+        Student st3 = new Student("최시나", 10209, 2);
 
         // 출석횟수가 -1회인 2학년 1반 1번 박마루를 생성합니다.
         // 출석횟수는 음수일 수 없다는 메시지가 출력되고 생성되지 않습니다.
+        try {
+            Student parkMaRu = new Student("박마루", 20101, -1);
+        } catch (IllegalArgumentException e) {
+            System.out.println("출석횟수는 음수일 수 없습니다.");
+        }
 
         // TODO-3 출석부에 학생 삽입
-        // 생성한 출석부에 이밤돌, 김이로를 삽입합니다.
+        // 생성한 출석부에 이밤돌, 김이로를 삽입
+        book.addStudent(st1);
+        book.addStudent(st2);
 
         // TODO-4 출석부에 동일한 학생 삽입
         // 출석부에 이밤돌을 다시 삽입합니다.
+        book.addStudent(st1);
 
         // 출석부에 이밤돌은 존재하므로 삽입할 수 없다는 메시지가 출력됩니다.
 
