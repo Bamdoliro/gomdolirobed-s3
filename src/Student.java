66184ee33c73d49f84b91726effb1addd41315c2
tmdahr <p84800384@gmail.com>
2025-12-01 06:26:43 +0900

    feat::학생 클레스 생성 - 학생 목록이 출석부 클레스 안에 있기 때문에 먼저 만듬

---
+++ src/Student.java
@@ -0,0 +1,18 @@
+public class Student implements Comparable<Student> {
+    private final String name;
+    private final int classNumber;
+    private int attendanceCount;
+
+    public Student(String name, int classNumber, int attendanceCount) {
+        if(name == null || name.length() < 1 || name.length() > 20)
+            throw new IllegalArgumentException("학생의 이름은 1자 이상 20자 이하이어야 합니다.");
+        if(attendanceCount < 0)
+            throw new IllegalArgumentException("출석 횟수는 음수일 수 없습니다.");
+
+        this.name = name;
+        this.classNumber = classNumber;
+        this.attendanceCount = attendanceCount;
+    }
+
+
+}
