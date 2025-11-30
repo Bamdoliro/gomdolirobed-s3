9c6a03bf5ff2407447709aca9e7bf8c5e8da42e4
여철준 <jun091027naver.com>
2025-12-01 08:25:53 +0900

    perf :: 메시지 한글로 수정

--- src/student/Student.java
+++ src/student/Student.java
@@ -6,7 +6,7 @@ public class Student {
     private Integer attendance;
 
     public Student(String name, Integer number, Integer attendance) {
-        if (attendance < 0) throw new IllegalArgumentException("Attendance cannot be negative");
+        if (attendance < 0) throw new IllegalArgumentException("출석 횟수는 음수일 수 없습니다.");
         this.name = name;
         this.number = number;
         this.attendance = attendance;
