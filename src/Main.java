3bc2735e034d67bd0b47ab930d4017a8877c79f6
tmdahr <p84800384@gmail.com>
2025-12-01 08:06:08 +0900

    feat:: TODO-5 구현 및 오류 메세지 수정 한번에 1만큼만 증가시킬 수 있다는 조건이 없어 원하는 만큼 증가시킬 수 있게 구현

--- src/Main.java
+++ src/Main.java
@@ -21,9 +21,9 @@ public class Main {
         // 출석횟수가 -1회인 2학년 1반 1번 박마루를 생성합니다.
         // 출석횟수는 음수일 수 없다는 메시지가 출력되고 생성되지 않습니다.
         try {
-            Student parkMaRu = new Student("박마루", 20101, -1);
+            Student st4u = new Student("박마루", 20101, -1);
         } catch (IllegalArgumentException e) {
-            System.out.println("출석횟수는 음수일 수 없습니다.");
+            System.out.println("잘못된 값을 전달했습니다");
         }
 
         // TODO-3 출석부에 학생 삽입
@@ -33,13 +33,18 @@ public class Main {
 
         // TODO-4 출석부에 동일한 학생 삽입
         // 출석부에 이밤돌을 다시 삽입합니다.
-        book.addStudent(st1);
+        try {
+            book.addStudent(st1);
+        } catch (IllegalArgumentException e) {
+            System.out.println("출석부에 동일한 학생은 추가할 수 없습니다");
+        }
 
         // 출석부에 이밤돌은 존재하므로 삽입할 수 없다는 메시지가 출력됩니다.
 
         // TODO-5 출석횟수 증가
         // 출석부에 존재하는 이밤돌, 김이로의 출석횟수가 각각 1씩 증가합니다.
-
+        book.increaseAttendance(st1, 1);
+        book.increaseAttendance(st2, 1);
 
         // TODO-6 출석부 조회
         // 출석부에 등록된 모든 학생의 정보를 조회합니다.
