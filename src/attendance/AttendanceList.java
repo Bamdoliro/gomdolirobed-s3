c49fc1207d5641d634b88dffb99ccc07f58e1e77
여철준 <jun091027naver.com>
2025-12-01 01:21:55 +0900

    feat :: 출석부 클래스 만듬 - 출석부 클래스 생성 및 속성과 메서드 만듬

--- src/attendance/AttendanceList.java
+++ src/attendance/AttendanceList.java
@@ -1,4 +1,48 @@
 package attendance;
 
+import student.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AttendanceList {
+    private final List<Student> students = new ArrayList<>();
+
+    public void addStudent(Student student) {
+        if (student.getName().length() > 20 && student.getName().isBlank()) throw new IllegalArgumentException("Name is too long or too short");
+        if (student.getAttendance() < 0) throw new IllegalArgumentException("Attendance cannot be negative");
+        for (Student s : students) {
+            if (s.getName().equals(student.getName()) && s.getNumber().equals(student.getAttendance())) {
+                throw new IllegalArgumentException("Student already exists");
+            }
+        }
+        students.add(student);
+    }
+
+    public AttendanceList(Student... students) {
+        for (Student student : students) {
+            this.addStudent(student);
+        }
+    }
+    public void deleteStudent(Student student) {
+        try {
+            students.remove(student);
+        } catch (IndexOutOfBoundsException e) {
+            throw  new IllegalArgumentException("Student not found");
+        }
+    }
+    public void addAttendance(Student student, Integer attendance) {
+        if (attendance < 0) throw new IllegalArgumentException("Attendance cannot be negative");
+        Integer oldAttendance = student.getAttendance();
+        for (Student s : students) {
+            if (s.equals(student)) student.setAttendance(oldAttendance + attendance);
+        }
+    }
+
+    public void printStudents() {
+        for (Student student : students) {
+            System.out.println("Student Name: " + student.getName() + "Student Number: " + student.getNumber() + "Student Attendance:" + student.getAttendance());
+        }
+    }
+
 }
